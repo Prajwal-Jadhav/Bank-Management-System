ac37569d6ee0b3307ba44145274e5611c7dd9896
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-12-07 14:08:31 +0530

    Corrected populate function signature

--- Account_storage.h
+++ Account_storage.h
@@ -4,13 +4,12 @@
 
 #include "Account.h"
 #include "Linked_list.h"
-#include <fstream>
 
 class Account_storage
 {
 	Account_storage(int size = 15);
 	~Account_storage();
-	void populate_from_record(std::string file_name);
+	void populate_from_record();
 	void add_account(Account acc);
 private:
 	Linked_list** list;
