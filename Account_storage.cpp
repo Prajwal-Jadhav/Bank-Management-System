f9fb8ad9a0588c50d2ad74b321ec30151b540b41
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-12-07 13:49:39 +0530

    Completed destructor

--- Account_storage.cpp
+++ Account_storage.cpp
@@ -1,14 +1,24 @@
 #include "Account_storage.h"
 
-Account_storage::Account_storage(int size = 15)
+// Constructor which reads input from  a 
+// text file to create accounts
+// Some code is remaining to be written
+Account_storage::Account_storage(int sz = 15)
 {
-	list = new Account * [size];
+	size = sz;
+	list = new Linked_list*[sz];
+
+	for (int i = 0; i < sz; ++i)
+		list[i] = new Linked_list;
 }
 
-// To do
-//Account_storage::~Account_storage()
-//{
-//	
-//}
+// Destructor for deallocating storage
+Account_storage::~Account_storage()
+{
+	for (int i = 0; i < size; ++i)
+		list[i]->clear();
+
+	delete[] list;
+}
 
 
