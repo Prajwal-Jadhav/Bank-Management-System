b7d3b8ed19962bb6f7d623c233a87443b0ea33bd
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-12-08 16:56:20 +0530

    Added menu driven functionality in main function

--- bank_management.cpp
+++ bank_management.cpp
@@ -6,6 +6,23 @@
 
 int main()
 {
-    // This function will be written after completing all the auxilliary classes.
+    std::cout << "*************  Welcome to Bank Management System  **************\n\n";
+    Account_storage accounts;
+
+    while (true)
+    {
+        std::cout << "Main menu: Press the digit to perform action\n";
+        std::cout << "0 - Exit\n";
+        std::cout << "1 - Search for account\n";
+        std::cout << "2 - Create new account\n";
+
+        int input;
+        std::cin >> input;
+
+        if (input == 0)
+            break;
+    }
+
+    std::cout << "\n************** Thank you for using bank management system  *****************\n";
 }
 
